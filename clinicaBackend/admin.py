07b6d501b7b8e36720bfd683ea3aa278557d52f1
Anghelum <anghelum3013@gmail.com>
2022-10-05 08:15:41 -0500

    pruebas

--- clinicaBackend/admin.py
+++ clinicaBackend/admin.py
@@ -4,7 +4,6 @@ from .models.medico import Medico
 from .models.paciente import Paciente
 from .models.historia_clinica import Historia_clinica
 from .models.enfermero import Enfermero
-from .models.enfermero_paciente import Enfermero_paciente
 
 # Register your models here.
 admin.site.register(Usuario)
@@ -12,5 +11,4 @@ admin.site.register(Medico)
 admin.site.register(Paciente)
 admin.site.register(Historia_clinica)
 admin.site.register(Enfermero)
-admin.site.register(Enfermero_paciente)
 
